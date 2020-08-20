58b8dde1578da5bfeb00e8299b7bc7a0ef12fb97
thenatural86 <philip.smalls86@gmail.com>
2020-08-20 16:02:12 -0400

    python time

---
+++ greeting.py
@@ -0,0 +1,5 @@
+def greet(name):
+    print("Hello, {}".format(name))
+
+
+greet("philip")
---
+++ input_demo.py
@@ -0,0 +1,5 @@
+# name = input("What's your name? ")
+# print("Hello, {}!".format(name.title()))
+
+x = eval(input("Enter an expression. "))
+print(x)
