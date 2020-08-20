d9c7fa6b0255cd287d1f9d1e7e406d4a0575adb5
thenatural86 <philip.smalls86@gmail.com>
2020-08-20 19:18:33 -0400

    for loop, split, zip

--- scripting_raw_input.py
+++ scripting_raw_input.py
@@ -1,8 +1,10 @@
-names = input("Enter names separated by commas: ").split()
-assignments = input("Enter assignment counts separated by commas:").split()
-grades = input("Enter grades separated by commas: ").split()
+names = input("Enter names separated by commas: ").split(",")
+assignments = input("Enter assignment counts separated by commas:").split(",")
+grades = input("Enter grades separated by commas: ").split(",")
 
-# message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
-# submit before you can graduate. You're current grade is {} and can increase \
-# to {} if you submit all assignments before the due date.\n\n"
-print(names)
+message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
+submit before you can graduate. You're current grade is {} and can increase \
+to {} if you submit all assignments before the due date.\n\n"
+
+for name, assignment, grade in zip(names, assignments, grades):
+    print(message.format(name, assignment, grade, grade + (assignment*2)))
