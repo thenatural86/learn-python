6fe5879ca1fabc2cecb57ee64351413dce4be0c0
thenatural86 <philip.smalls86@gmail.com>
2020-08-20 17:04:48 -0400

    input

--- scripting_raw_input.py
+++ scripting_raw_input.py
@@ -1,3 +1,8 @@
-name = input("Enter names separated by commas: ")
-assignments = input("Enter assignment counts separated by commas:")
-grades = input("Enter grades separated by commas: ")
+names = input("Enter names separated by commas: ").split()
+assignments = input("Enter assignment counts separated by commas:").split()
+grades = input("Enter grades separated by commas: ").split()
+
+# message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
+# submit before you can graduate. You're current grade is {} and can increase \
+# to {} if you submit all assignments before the due date.\n\n"
+print(names)
